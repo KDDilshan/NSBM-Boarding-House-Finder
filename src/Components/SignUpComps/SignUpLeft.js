80c5ed8405e3180cb15edd46f0e0bbe72ba77652
Hirunidiv <hirunidivyanjalee365@gmail.com>
2024-01-14 23:38:27 +0530

    create SignUp page

---
+++ src/Components/SignUpComps/SignUpLeft.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './signUpLeft.css'
+
+export default function SignUpLeft() {
+  return (
+    <div className='imageContainer'>
+      <img src="./images/formImage.svg" alt="" className="imgOne" />
+    </div>
+  )
+}
--- src/Pages/SignUpPage/SignUp.js
+++ src/Pages/SignUpPage/SignUp.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './signup.css'
+import SignUpRight from '../../Components/SignUpComps/SignUpRight'
+import SignUpLeft from '../../Components/SignUpComps/SignUpLeft'
 
 export default function SignUp() {
   return (
-    <div>
-      
+    <div className='pageContainer'>
+      <SignUpLeft/>
+      <SignUpRight/>
     </div>
   )
 }
