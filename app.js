80950e3b61c467dc82675bea6419ade732332434
KDDilshan <kdprojects847@gmail.com>
2024-01-15 17:00:30 +0530

    CHANGE SOMETHINIG

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import pool from './config/db.js';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import {usermethods} from './routes/user.js'
 import {ownerDescription} from './routes/boardingHouse.js';
 
-
-
+dotenv.config()
 
 const app=express()
 const port=4000
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use('/form',usermethods)
 app.use('/owner',ownerDescription)
 
-mongoose.connect('mongodb+srv://root:123456root@nodeexpresssprojects.dd45m8n.mongodb.net/Documents?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGODB_KEY)
 .then(()=>{
     console.log('connected to mongose')
     app.listen(port,()=>{
