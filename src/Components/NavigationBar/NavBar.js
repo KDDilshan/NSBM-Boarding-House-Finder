e8c90d9cf3ef88f68d9550ead2c76ad48447d7a6
Hirunidiv <hirunidivyanjalee365@gmail.com>
2024-01-16 10:20:54 +0530

    create navigation bar.

--- src/Components/NavigationBar/NavBar.js
+++ src/Components/NavigationBar/NavBar.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './navBar.css'
+import HomeIcon from '@mui/icons-material/Home';
 
 export default function NavBar() {
   return (
-    <div>
-      
+    <div className='NavBarBox'>
+            <div className="navBarLeft">
+                <span className="logo">campus bodima</span>
+            </div>
+            <div className="navBarRight">
+              <div className="navBarIcons">
+                    <HomeIcon className='homeicon'/>
+                    <div className="iconButtons">
+                        <a href='/' className="logbutton">Log in</a>
+                        <a href="/SignUp" className="signinbutton">Sign up</a>
+                        <span className="aboutbutton">About us</span>
+                    </div>
+                     
+              </div>
+            </div>
     </div>
   )
 }
